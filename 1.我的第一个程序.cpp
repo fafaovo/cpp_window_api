3f32fed4c63699db975e118fac55aa6ee5cb635a
fafaovo <aa1206180803@sina.cn>
2021-04-16 23:24:14 +0800

    2021-4-16

---
+++ 1.我的第一个程序.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+#include <Windows.h>
+
+
+/*winMain相当于main*/
+/*参数(当前程序的实例句柄,上一个程序的实例句柄,命令行参数)*/
+/*需求修改字符集改为多字节
+左转字符串宽窄函数前两节
+*/
+int WinMain(HINSTANCE hInstance, HINSTANCE hPreInstance, char* ipCmdLine, int nshowCmd)
+{
+	return 0;
+}
